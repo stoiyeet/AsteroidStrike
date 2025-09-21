1da1c51eaadbe48188d69d1716aa7bb2acd76f2e
stoiyeet <markisaackogan@gmail.com>
2025-09-20 22:09:24 -0400

    Adjust custom meteor model based on input size

--- components/meteors/EarthImpact.tsx
+++ components/meteors/EarthImpact.tsx
@@ -94,8 +94,7 @@ export default function EarthImpact({
 
   const asteroidScale = useMemo(() => {
     if (meteor.isCustom) {
-      // For custom asteroids, we use the sphere directly at the desired size
-      return 1;
+      return meteor.diameter/20000;
     }
     if (!gltf) return 1;
     const box = new THREE.Box3().setFromObject(gltf.scene);
@@ -197,7 +196,7 @@ export default function EarthImpact({
           {/* Render custom sphere or GLB model */}
           {meteor.isCustom ? (
             <mesh>
-              <sphereGeometry args={[desiredAsteroidRadiusUnits * 2, 32, 32]} />
+              <sphereGeometry args={[desiredAsteroidRadiusUnits * asteroidScale, 32, 32]} />
               <meshStandardMaterial
                 color={customMaterial?.color || '#8B7355'}
                 metalness={customMaterial?.metalness || 0.2}
