348ced132c7c4e5ead47064fa2264028af77a180
stoiyeet <markisaackogan@gmail.com>
2025-10-06 01:25:56 -0400

    change name to avoid collision

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata = {
     url: "https://asteroidstrike.earth",
     images: [
       {
-        url: "https://asteroidstrike.earth/appHeader/promotion.png",
+        url: "https://asteroidstrike.earth/appHeader/clickbait.png",
         width: 1200,
         height: 630,
       },
